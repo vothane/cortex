c114fa9ca57cd18ec8ffc5fcf8207c81ec310b5e
Thane Vo <vothane@gmail.com>
2019-10-31 18:40:40 -0400

    wire up a deep learner for XOR problem to see how things run, even tho accurracy is bad the DL operates correctly

--- test/neural_network_test.exs
+++ test/neural_network_test.exs
@@ -20,8 +20,36 @@ defmodule NeuralNetworkTest do
     NeuralNetwork.add(nn, Dense.dense(%{n: 1}))
     NeuralNetwork.add(nn, sigmoid_layer)
     
-    out = NeuralNetwork.forward_propogate(nn, Matrex.new([[1, 0]]))
-    NeuralNetwork.backward_propogate(nn, out)
-  end
+    training_set = [{Matrex.new([[0, 0]]), Matrex.new([[0]])}, 
+                    {Matrex.new([[0, 1]]), Matrex.new([[1]])}, 
+                    {Matrex.new([[1, 0]]), Matrex.new([[1]])}, 
+                    {Matrex.new([[1, 1]]), Matrex.new([[0]])}]
 
+    Enum.reduce(1..5000, [], fn(_, _) -> 
+      Enum.reduce(training_set, [], fn({x, y}, _) ->
+        outputs = NeuralNetwork.forward_propogate(nn, x)
+        output_deltas = Matrex.apply(outputs, fn y_output -> y_output * (1 - y_output) * (y_output - Matrex.at(y, 1, 1)) end)
+        NeuralNetwork.backward_propogate(nn, output_deltas)
+      end)
+    end)
+    
+    is_within? = fn (y, actual, delta) -> (actual - delta) < y and (actual + delta) > y end
+    
+    y_0_0 = NeuralNetwork.forward_propogate(nn, Matrex.new([[0, 0]]))
+    y_0_1 = NeuralNetwork.forward_propogate(nn, Matrex.new([[0, 1]]))
+    y_1_0 = NeuralNetwork.forward_propogate(nn, Matrex.new([[1, 0]]))
+    y_1_1 = NeuralNetwork.forward_propogate(nn, Matrex.new([[1, 1]]))
+    
+    # tolerance here is generous and indicates that deep learner architecture is not
+    # optimal, my guess is that the tanh activation layer shouldn't be used b/c the
+    # tanh function is known to have some issues in some cases
+    # but now I'm just concern with the DL working correctly not accuracy
+    
+    tolerance = 0.25
+    
+    assert is_within?.(Matrex.at(y_0_0, 1, 1), 0, tolerance)
+    assert is_within?.(Matrex.at(y_0_1, 1, 1), 1, tolerance)
+    assert is_within?.(Matrex.at(y_1_0, 1, 1), 1, tolerance)
+    assert is_within?.(Matrex.at(y_1_1, 1, 1), 0, tolerance)
+  end
 end

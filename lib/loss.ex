56cfd8dbad26c267805f76dbdad27fc3bbaf3be4
Thane Vo <vothane@gmail.com>
2020-04-14 17:31:43 -0600

    fix dynamic dispatching of loss functions

--- lib/loss.ex
+++ lib/loss.ex
@@ -3,13 +3,13 @@ defmodule Loss do
   @callback gradient!(struct, any, any) :: any
   
   def loss(loss, actuals, predictions) do
-    %module{} = loss
-    module.loss!(loss, actuals, predictions)   
+    %module{} = Agent.get(loss, &(&1))
+    module.loss!(actuals, predictions)   
   end
   
   def gradient(loss, actuals, predictions) do
-    %module{} = loss
-    module.gradient!(loss, actuals, predictions)   
+    %module{} = Agent.get(loss, &(&1))
+    module.gradient!(actuals, predictions)   
   end
 end
 
@@ -19,10 +19,10 @@ defmodule SquareLoss do
   @behaviour Loss
   
   @impl Loss
-  def loss!(loss, y, y_pred), do: Matrex.multiply(0.5, Matrex.apply(Matrex.subtract(y, y_pred), fn x -> :math.pow(x, 2) end))
+  def loss!(y, y_pred), do: Matrex.multiply(0.5, Matrex.apply(Matrex.subtract(y, y_pred), fn x -> :math.pow(x, 2) end))
   
   @impl Loss
-  def gradient!(loss, y, y_pred), do: Matrex.multiply(-1, (Matrex.subtract(y, y_pred)))
+  def gradient!(y, y_pred), do: Matrex.multiply(-1, (Matrex.subtract(y, y_pred)))
 
   def square_loss(%{}) do
     Agent.start_link(fn -> %SquareLoss{} end)
@@ -35,7 +35,7 @@ defmodule CrossEntropy do
   @behaviour Loss
 
   @impl Loss
-  def loss!(loss, y, p) do
+  def loss!(y, p) do
     p = Utils.clip(p, 1.0e-15, 1-1.0e-15)
     a = Matrex.apply(Matrex.multiply(y, Matrex.apply(p, :log)), fn x -> -1 * x end)
     b = Matrex.multiply(Matrex.subtract(1, y), Matrex.apply(Matrex.subtract(1, p), :log))
@@ -43,7 +43,7 @@ defmodule CrossEntropy do
   end
   
   @impl Loss
-  def gradient!(loss, y, p) do
+  def gradient!(y, p) do
     p = Utils.clip(p, 1.0e-15, 1-1.0e-15)
     a = Matrex.apply(Matrex.divide(y, p), fn x -> -1 * x end)
     b = Matrex.divide(Matrex.subtract(1, y), Matrex.subtract(1, p))
